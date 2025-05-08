97ed36d80fe2c78927ae2c5958c67e7065795961
Lorien Escobedo <blacktiger6254@gmail.com>
2025-05-08 16:02:52 -0500

    Organized a bit & started working on opponent motorcycle ai & added the obstacles

--- src/project.py
+++ src/project.py
@@ -6,12 +6,20 @@ import random
 WIDTH, HEIGHT = 320, 240
 GRID_SIZE = 2 
 
-POSSIBLE_CAR_POINTS = [
-    (x, y) for x in [23, 100, 143, 226, 265]
-             for y in [55, 78, 100, 122, 143, 165, 186]
-]
+def get_random_direction():
+    DIRECTIONS = {
+    "LEFT": (-GRID_SIZE, 0),
+    "RIGHT": (GRID_SIZE, 0),
+    "UP": (0, -GRID_SIZE),
+    "DOWN": (0, GRID_SIZE)
+    }
+    return random.choice(["LEFT", "RIGHT", "UP", "DOWN"])
 
 def generate_cars(count):
+    POSSIBLE_CAR_POINTS = [
+    (x, y) for x in [23, 100, 143, 226, 265]
+             for y in [55, 78, 100, 122, 143, 165, 186]
+    ]
     return random.sample(POSSIBLE_CAR_POINTS, count) 
 
 def main():
@@ -21,20 +29,23 @@ def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Rev Up!")
     parking = pygame.image.load("parking_lot.jpg")
-    blue_car = pygame.image.load("blue_car.png")
-    orange_car = pygame.image.load("orange_car.png")
-    purple_car = pygame.image.load("purple_car.png")
 
-    obstacle_cars = [blue_car, orange_car, purple_car]
+    obstacle_cars = [
+        pygame.image.load("blue_car.png"),
+        pygame.image.load("orange_car.png"),
+        pygame.image.load("purple_car.png")
+    ]
     obstacles = [(x, y, random.choice(obstacle_cars)) for x, y in generate_cars(5)]
 
 
     motorcycle_opt = pygame.image.load("green_motorcycle.png")
-    opt_x, opt_y = 0, -1
-
-    motorcycle_player = pygame.image.load("red_motorcycle.png")
+    opt_x, opt_y = WIDTH // 4, HEIGHT // 4
+    opt_angle = 0
+    opt_direction = get_random_direction()
+    
+    motorcycle_play = pygame.image.load("red_motorcycle.png")
     play_x, play_y = WIDTH // 2, HEIGHT // 2
-    angle = 0
+    play_angle = 0
     direction = "UP"
     moving = False
 
@@ -47,34 +58,35 @@ def main():
                 moving = True
                 if event.key == pygame.K_LEFT:
                     direction = "LEFT"
-                    angle = 90
+                    play_angle = 90
                 elif event.key == pygame.K_RIGHT:
                     direction = "RIGHT"
-                    angle = -90
+                    play_angle = -90
                 elif event.key == pygame.K_UP:
                     direction = "UP"
-                    angle = 0
+                    play_angle = 0
                 elif event.key == pygame.K_DOWN:
                     direction = "DOWN"
-                    angle = 180
+                    play_angle = 180
 
         if moving:
             if direction == "LEFT" and play_x - GRID_SIZE >= 5:
                 play_x -= GRID_SIZE
-            elif direction == "RIGHT" and play_x + GRID_SIZE <= WIDTH - motorcycle_player.get_width():
+            elif direction == "RIGHT" and play_x + GRID_SIZE <= WIDTH - motorcycle_play.get_width():
                 play_x += GRID_SIZE
             elif direction == "UP" and play_y - GRID_SIZE >= 7:
                 play_y -= GRID_SIZE
-            elif direction == "DOWN" and play_y + GRID_SIZE <= HEIGHT - motorcycle_player.get_width():
+            elif direction == "DOWN" and play_y + GRID_SIZE <= HEIGHT - motorcycle_play.get_width():
                 play_y += GRID_SIZE
 
-
-        rotated_motorcycle = pygame.transform.rotate(motorcycle_player, angle)
-        rect = rotated_motorcycle.get_rect(center=(play_x, play_y))
+        rotated_opt_motorcycle = pygame.transform.rotate(motorcycle_opt, opt_angle)
+        rect_opt = rotated_opt_motorcycle.get_rect(center=(opt_x, opt_y))
+        rotated_play_motorcycle = pygame.transform.rotate(motorcycle_play, play_angle)
+        rect = rotated_play_motorcycle.get_rect(center=(play_x, play_y))
 
         screen.blit(parking, (0, 0))
-        screen.blit(motorcycle_opt, (opt_x, opt_y))
-        screen.blit(rotated_motorcycle, rect.topleft)
+        screen.blit(rotated_opt_motorcycle, rect_opt.topleft)
+        screen.blit(rotated_play_motorcycle, rect.topleft)
         pygame.display.flip()
         clock.tick(60)
     pygame.quit()
